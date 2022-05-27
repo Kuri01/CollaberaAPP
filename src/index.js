13eecf3956227466f0713e1fb4b32e1f0ca096a8
Kuri01 <kamil.kuriata01@gmail.com>
2022-05-27 10:26:05 +0200

    Create initial components

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import MainContainer from './_MainContainer/_MainContainer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <MainContainer />
   </React.StrictMode>
 );
 
