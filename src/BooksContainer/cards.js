fb90f4cdc091d69bdf765959dffbc5d3b9b84dee
Kuri01 <kamil.kuriata01@gmail.com>
2022-05-28 16:21:54 +0200

    End product page

---
+++ src/BooksContainer/cards.js
@@ -0,0 +1,51 @@
+import image1 from '../Images/image1.jpg';
+import image2 from '../Images/image2.jpg';
+import image3 from '../Images/image3.jpg';
+import image4 from '../Images/image4.jpg';
+import image5 from '../Images/image5.jpg';
+import image6 from '../Images/image6.jpeg';
+import image7 from '../Images/image7.jpg';
+import image8 from '../Images/image8.jpg';
+
+export default [
+  {
+    url: image1,
+    title: 'Norwegian Wood',
+    text: 'Nec tempor mi. Aenean euismod nunc ligula, ut tincidunt elit pretium at. Integer a molestie purus. Aliquam erat volutpat. Ut id urna eget velit ullamcorper cursus.',
+  },
+  {
+    url: image2,
+    title: 'Kafka on the Shore',
+    text: 'Vitae pulvinar tincidunt. Donec vel magna nunc. Cras pharetra cursus lectus, a pharetra orci iaculis non. Quisque eget mauris in lorem sodales gravida in sit amet ligula.',
+  },
+  {
+    url: image3,
+    title: 'After Dark',
+    text: 'Condimentum leo, vel fermentum massa pulvinar vitae. Nam a felis libero. Fusce pellentesque dignissim finibus. Nulla hendrerit vulputate nisl, nec aliquam elit mollis eu.',
+  },
+  {
+    url: image4,
+    title: '1Q84',
+    text: 'Massa vitae ornare vestibulum, ligula elit ultrices mi, nec scelerisque odio nulla ut eros. Nam sit amet sem sit amet augue dictum convallis. Integer congue tortor dui.',
+  },
+  {
+    url: image5,
+    title: 'History of Greece',
+    text: 'Integer at hendrerit odio. Quisque non enim tellus. Etiam commodo quam quis eleifend ullamcorper.',
+  },
+  {
+    url: image6,
+    title: 'The Positive Dog',
+    text: 'usce auctor, risus eget laoreet dictum, justo odio venenatis quam, et porta tellus ante ac purus.',
+  },
+  {
+    url: image7,
+    title: 'The Scribe Method',
+    text: 'Mauris ornare mattis eros vitae iaculis. Fusce blandit eget enim eu tempor. Donec in risus nec augue pharetra ornare. Phasellus dignissim mi eu nulla malesuada bibendum.',
+  },
+  {
+    url: image8,
+    title: 'The Herald of Coming Good',
+    text: 'Consectetur adipiscing elit. Cras euismod scelerisque nisl id viverra. Vivamus vestibulum turpis nec pellentesque convallis.',
+  },
+];
--- src/_MainContainer/_MainContainer.js
+++ src/_MainContainer/_MainContainer.js
@@ -1,18 +1,34 @@
-import React from 'react';
-import { Books } from '../BooksContainer/BooksContainer';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { mockBooks } from '../data/data';
+import BooksContainer from '../BooksContainer/BooksContainer';
+import Header from '../Header/Header';
+import React from 'react';
+import HomePage from '../HomePage/HomePage';
+
 class MainContainer extends React.Component {
   constructor(props) {
     super(props);
   }
   state = {
     books: mockBooks,
+    isLoading: true,
   };
+
+  componentDidMount() {
+    this.setState({ ...this.state, isLoading: false });
+  }
 
   render() {
     return (
       <div>
-        <Books books={this.state.books} />
+        <BrowserRouter>
+          {' '}
+          <Header />
+          <Routes>
+            <Route path='/' element={<HomePage title={'title'} />} />
+            <Route path='/products' element={<BooksContainer />} />
+          </Routes>
+        </BrowserRouter>
       </div>
     );
   }
